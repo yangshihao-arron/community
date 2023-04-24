81303d3e8b71b2ceb4c71bc0745df119e3a72635
yangshihao-arron <2867351265@qq.com>
2023-04-24 19:11:19 +0800

    add htmltopdf function

--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -30,9 +30,10 @@ public interface CommunityConstant {
     String TOPIC_LIKE = "like";
     //主题:关注
     String TOPIC_FOLLOW = "follow";
-
     //主题:发帖
     String TOPIC_PUBLISH = "publish";
+    // 主题:分享
+    String TOPIC_SHARE = "share";
 
     // 系统用户ID
     int SYSTEM_USER_ID = 1;
